a932e9ff24a4e40f68cb99a19e48415cc256a8df
1jsingh <jaskirat.singh@anu.edu.au>
2025-10-21 14:46:32 -0700

    update

--- repa-e-t2i/serve.sh
+++ repa-e-t2i/serve.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Simple HTTP server for local preview of the project page
 
-PORT=8002
+PORT=8005
 
 echo "Starting local server on http://localhost:$PORT"
 echo "Press Ctrl+C to stop"
